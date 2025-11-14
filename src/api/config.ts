1108f509b00d65d4a83ca39cd2ae61f0c288efe1
nikhlSaini0210 <nikhilsaini0210@gmail.com>
2025-11-14 19:59:14 +0530

    update code

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'http://testlink4.pillersofttechnologies.com/api';
+export const API_BASE_URL = 'https://testlink4.pillersofttechnologies.com/api';
 
 export const TIMEOUT = 30000;
 
